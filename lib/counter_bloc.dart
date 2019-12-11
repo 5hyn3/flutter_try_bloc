d3be523eccf0be329aba58c4e32acd4374d26ad1
shyne <shyne9795@gmail.com>
2019-12-12 01:50:15 +0900

    separete function

--- lib/counter_bloc.dart
+++ lib/counter_bloc.dart
@@ -10,11 +10,12 @@ class CounterBloc {
   int _count = 0;
 
   CounterBloc() {
-    _actionController.stream.listen((_) {
-      // この部分に各種計算やAPIアクセスなどのロジックを記述する。
-      _count++;
-      _countController.sink.add(_count);
-    });
+    _actionController.stream.listen(_handler);
+  }
+
+  void _handler(data) {
+    _count++;
+    _countController.sink.add(_count);
   }
 
   void dispose() {
